c411a1dd59d67c7c3877296af0f244cfbff8f199
Marlon Almeida <soikmd2@gmail.com>
2020-07-25 09:18:54 -0300

    Initial commit

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyFirstTest {
+
+    @Test
+    public void myFirstTest(){
+        boolean result = true;
+        Assertions.assertTrue(result);
+    }
+}
