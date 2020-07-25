0026116176541b03fbc368766ca472dd60d80a20
Marlon Almeida <soikmd2@gmail.com>
2020-07-25 14:45:16 -0300

    Adjusts palindrome tests, simplify Palindrome class

--- src/main/java/school/cesar/eta/unit/Palindrome.java
+++ src/main/java/school/cesar/eta/unit/Palindrome.java
@@ -4,15 +4,12 @@ public class Palindrome {
 
     public boolean check(String word){
         int length = word.length();
-
         if (length <= 1){
             return true;
         }
-
-        if(word.charAt(0) == word.charAt(length-1)){
-            return check(word.substring(1,length-1));
+        if(word.charAt(0) != word.charAt(length -1)){
+            return false;
         }
-
-        return false;
+        return check(word.substring(1,length -1));
     }
 }
--- src/test/java/school/cesar/eta/unit/PalindromeTest.java
+++ src/test/java/school/cesar/eta/unit/PalindromeTest.java
@@ -1,29 +1,32 @@
 package school.cesar.eta.unit;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PalindromeTest {
+    private Palindrome palindrome;
+
+    @BeforeEach
+    public void setupTest(){
+        palindrome = new Palindrome();
+    }
 
     @Test
-    public void check_palindromeWord_true(){
-        Palindrome palindrome = new Palindrome();
-        boolean actual = palindrome.check("ovo");;
+    public void check_OneCharWord_true(){
+        boolean actual = palindrome.check("a");
         Assertions.assertTrue(actual);
     }
 
     @Test
     public void check_palindromeWord_false(){
-        Palindrome palindrome = new Palindrome();
-        boolean actual = palindrome.check("ovos");;
+        boolean actual = palindrome.check("ab");
         Assertions.assertFalse(actual);
     }
 
     @Test
-    public void check_OneChar_true(){
-        Palindrome palindrome = new Palindrome();
-        boolean actual = palindrome.check("o");;
+    public void check_palindromeWord_true(){
+        boolean actual = palindrome.check("racecar");
         Assertions.assertTrue(actual);
-
     }
 }
